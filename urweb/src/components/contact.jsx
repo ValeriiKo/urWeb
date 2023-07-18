8909b0be7f178351dd83265d24441daef4d3fa0b
Valerii <valeriiko@outlook.com>
2023-07-18 14:43:48 +0300

    content

--- urweb/src/components/contact.jsx
+++ urweb/src/components/contact.jsx
@@ -149,9 +149,9 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
+            &copy; 2023 Design by{" "}
+            <a href="https://unipinstudio.com/" rel="nofollow">
+              UniPin WebStudio
             </a>
           </p>
         </div>
